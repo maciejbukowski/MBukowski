9b1e94a1ed1762f7bc36150a277264e36d9e2767
Maciej Bukowski <mbukow2@gmail.com>
2015-09-20 17:12:32 -0500

    Added admin area and controllers

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MBukowski.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,11 +14,11 @@ namespace MBukowski
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            var namespaces = new[] { typeof(PostsController).Namespace};
+
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
         }
     }
 }
